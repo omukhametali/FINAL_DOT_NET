721312634d96b918ee560632c338b338309f5b2b
omukhametali <omuhametali@gmail.com>
2020-05-25 11:13:16 +0600

    Добавьте файлы проекта.

---
+++ FINAL_DOT_NET/Controllers/RoleController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FINAL_DOT_NET.Controllers
+{
+    public class RoleController : Controller
+    {
+        RoleManager<IdentityRole> roleManager;
+
+        /// 
+        /// Injecting Role Manager
+        /// 
+        /// 
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = roleManager.Roles.ToList();
+            return View(roles);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new IdentityRole());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(IdentityRole role)
+        {
+            await roleManager.CreateAsync(role);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FINAL_DOT_NET/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FINAL_DOT_NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
